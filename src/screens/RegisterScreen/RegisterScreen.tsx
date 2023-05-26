c88e5781e8a81afd30245577fe8ba32d4a28a147
Chi Nguyen Dac <dacchi6761@gmail.com>
2023-05-26 22:52:23 +0700

    formData format

--- src/screens/RegisterScreen/RegisterScreen.tsx
+++ src/screens/RegisterScreen/RegisterScreen.tsx
@@ -1,5 +1,139 @@
+import { Button, Col, DatePicker, Form, Input, Row } from 'antd';
+import { Dayjs } from 'dayjs';
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
 const RegisterScreen = () => {
-  return <div className="text-red-400 text-xl">Register Screen</div>;
+  const onFinish = (values: {
+    email: string;
+    username: string;
+    password: string;
+    phone_number: string;
+    date_of_birth: Dayjs;
+  }) => {
+    const formData = { ...values, date_of_birth: values.date_of_birth.format('DD-MM-YYYY') };
+    console.log({ formData });
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Row>
+      <Col className="mb-5 flex justify-center text-lg text-red-500" xs={24}>
+        Register Form
+      </Col>
+      <Col xs={24}>
+        <Row className="flex justify-center">
+          <Col xs={12}>
+            <Form {...formItemLayout} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+              <Form.Item
+                name="email"
+                label="E-mail"
+                rules={[
+                  {
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                  },
+                  {
+                    required: true,
+                    message: 'Please input your E-mail!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                label="Password"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                ]}
+                hasFeedback
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item
+                name="confirm"
+                label="Confirm Password"
+                dependencies={['password']}
+                hasFeedback
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please confirm your password!',
+                  },
+                  ({ getFieldValue }) => ({
+                    validator(_, value) {
+                      if (!value || getFieldValue('password') === value) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                    },
+                  }),
+                ]}
+              >
+                <Input.Password />
+              </Form.Item>
+              <Form.Item
+                name="username"
+                label="Username"
+                tooltip="Ur username!"
+                rules={[{ required: true, message: 'Please input your username!', whitespace: true }]}
+              >
+                <Input />
+              </Form.Item>
+              <Form.Item
+                name="phone_number"
+                label="Phone Number"
+                rules={[{ required: true, message: 'Please input your phone number!' }]}
+              >
+                <Input style={{ width: '100%' }} />
+              </Form.Item>
+              <Form.Item
+                name="date_of_birth"
+                label="DatePicker"
+                rules={[{ required: true, message: 'Please choosae your date of birth!' }]}
+              >
+                <DatePicker />
+              </Form.Item>
+              <Form.Item {...tailFormItemLayout}>
+                <Button type="primary" htmlType="submit">
+                  Register
+                </Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+  );
 };
 
 export default RegisterScreen;
